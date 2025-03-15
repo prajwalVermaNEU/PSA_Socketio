168f20fe4494884bfe54bcbde521021dfb259205
prajwalVermaNEU <144559798+prajwalVermaNEU@users.noreply.github.com>
2025-03-14 22:57:57 -0400

    Update server.py

--- server/server.py
+++ server/server.py
@@ -14,19 +14,23 @@ app.mount("/", socketio.ASGIApp(sio, app))
 
 # connections
 connected_clients, match_count = {}, 0
+active_students = defaultdict( list )
 def create_room():
     return "ROOM_" + str(random.randint( 100, 999))
 
-@sio.event
 async def join_room(sid, room_name):
     await sio.enter_room(sid, room_name)
-    print(f"Client {sid} joined room: {room_name}")
-    # await sio.emit("room_message", {"message": f"{sid} joined {room_name}"}, room=room_name)
+    print(f"Student {sid} joined room: {room_name}")
+
+WIDTH = 800
+HEIGHT = 600
+SEG_SIZE = 20
+def create_apple():
+    apple_posx = SEG_SIZE * random.randint(1, (WIDTH - SEG_SIZE) // SEG_SIZE)
+    apple_posy = SEG_SIZE * random.randint(1, (HEIGHT - SEG_SIZE) // SEG_SIZE)
+    return (apple_posx, apple_posy)
 
 async def create_match( currLevel):
-    print(f"*******************************")
-    print(active_students)
-    print(connected_clients)
     if len( active_students[currLevel]) > 1:
         # pop the last two elements and then add them into a room, notify them and finaly start the match ...
         player_1 = active_students[currLevel].pop()
@@ -35,24 +39,38 @@ async def create_match( currLevel):
         currRoom = create_room()
         await join_room( player_1, currRoom)
         await join_room( player_2, currRoom)
-        # match_count += 1
-        # await sio.emit(f"NEW MATCH: {match_count} between {player_1} and {player_2}")
-    
-active_students = defaultdict( list )
+
+        # Notify both players about each other
+        await sio.emit("MATCH_STARTED", {
+            "room": currRoom,
+            "player_1": player_1,
+            "player_2": player_2,
+            "APPLE_POS": create_apple()
+        }, room=currRoom)
+    else:
+        await sio.emit( "response", "SERVER: Kindly wait for sometime, we will find a pair for you to play")
+
+@sio.event
+async def update_snake_position( sid, data):
+    room, position = data["room"], data["position"]
+    print("Sending the coors from:", sid, " of :", position)
+    await sio.emit("opponent_snake_position", {
+        "player":sid,
+        "position": position
+    }, room = room, skip_sid=sid)
+
+
 @sio.event
 async def connect(sid, env, *args):
     print(f"Student: {sid} connected")
     query_string = env.get("QUERY_STRING", "")
     query_params = parse_qs(query_string)
-    print("1")
     NU_ID = query_params.get("NU_ID", ["XXXXXXXXX"])[0]
     MAIL_ID = query_params.get("MAIL_ID", ["guest@northeastern.com"])[0]
-    print("2")
     connected_clients[sid] = {
         "NU_ID":NU_ID,
         "MAIL_ID":MAIL_ID,
     }
-    print("3")
     # await sio.emit("USER-CONNECTED", {"sid": sid}, to=None, skip_sid=sid)
     active_students[0].append( sid )
     await create_match(0)
@@ -72,9 +90,10 @@ async def leave_room(sid, room_name):
 
 
 @sio.event
-async def message():
-    # await sio.emit("Prajwal this is a response.")
-    print("Here for good...")
+async def response(sid, data):
+    print(f"Got a msg from {sid} {data}")
+    # 1. Have the follow up match if possible and also notify the user to wait till the time comes
+    
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
